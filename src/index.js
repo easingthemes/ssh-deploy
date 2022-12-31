45bf2662807fef79892cf5cfef085ee74c83383d
Dragan Filipovic <info@frontenddot.com>
2022-12-31 10:30:19 +0100

    Remove Key log

--- src/index.js
+++ src/index.js
@@ -20,7 +20,6 @@ const defaultOptions = {
 console.log('GITHUB_WORKSPACE: ', GITHUB_WORKSPACE);
 console.log('REMOTE_HOST: ', process.env.REMOTE_HOST);
 console.log('REMOTE_USER: ', process.env.REMOTE_USER);
-console.log('SSH_PRIVATE_KEY: ', process.env.SSH_PRIVATE_KEY);
 
 const sshDeploy = (() => {
   const rsync = ({ privateKey, port, src, dest, args, exclude }) => {

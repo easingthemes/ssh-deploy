aeee708bee955b334c6ea09586924f8ba071fa01
Dragan Filipovic <info@frontenddot.com>
2022-12-31 02:43:17 +0100

    test

--- src/inputs.js
+++ src/inputs.js
@@ -4,6 +4,9 @@ const inputs = {
   GITHUB_WORKSPACE: process.env.GITHUB_WORKSPACE
 };
 // Get inputs from ENV or WITH workflow settings
+console.log('EXAMPLE_REMOTE_HOST: ', process.env.EXAMPLE_REMOTE_HOST);
+console.log('REMOTE_HOST: ', process.env.REMOTE_HOST);
+
 inputNames.forEach((input) => {
   inputs[input] = process.env[input] || process.env[`INPUT_${input}`];
 });

d85914e1f952ab565234f6091b0f022e0cc02ff5
Dragan Filipovic <info@frontenddot.com>
2022-12-31 03:15:04 +0100

    test

--- src/test.js
+++ src/test.js
@@ -1,2 +1,3 @@
 console.log('EXAMPLE_REMOTE_HOST: ', process.env.EXAMPLE_REMOTE_HOST);
 console.log('REMOTE_HOST: ', process.env.REMOTE_HOST);
+console.log('REMOTE_USER: ', process.env.REMOTE_USER);
